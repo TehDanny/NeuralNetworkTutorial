a8c369b41a284268151d902abed7242c4eea40f8
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2017-05-27 19:59:04 +0200

    https://www.youtube.com/watch?v=6lwM7I4vRPQ&list=PL29C61214F2146796&index=11 Start

--- NeuralNetwork/NeuralNetwork.cs
+++ NeuralNetwork/NeuralNetwork.cs
@@ -6,6 +6,57 @@ using System.Threading.Tasks;
 
 namespace NeuralNetwork
 {
+    #region Fransfer functions and their derivatives
+
+    public enum TransferFunction
+    {
+        None,
+        Sigmoid
+    }
+
+    static class TransferFunctions
+    {
+        public static double Evaluate(TransferFunction tFunc, double input)
+        {
+            switch (tFunc)
+            {
+                case TransferFunction.Sigmoid:
+                    return sigmoid(input);
+
+                case TransferFunction.None:
+                default:
+                    return 0.0;
+            }
+        }
+
+        public static double EvaluateDerivative(TransferFunction tFunc, double input)
+        {
+            switch (tFunc)
+            {
+                case TransferFunction.Sigmoid:
+                    return sigmoidDerivative(input);
+
+                case TransferFunction.None:
+                default:
+                    return 0.0;
+            }
+        }
+
+        //Transfer function definitions
+
+        private static double sigmoid(double x)
+        {
+            return 1.0 / (1.0 + Math.Exp(-x));
+        }
+
+        private static double sigmoidDerivative(double x)
+        {
+            return sigmoid(x) * (1 - sigmoid(x));
+        }
+    }
+
+    #endregion
+
     public class BackPropagationNetwork
     {
         public BackPropagationNetwork()
